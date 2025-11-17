f8c9255e148150c591d385a883587d04a410639b
ilfpns <yeomsh09@gmail.com>
2025-11-17 13:31:40 +0900

    imu get func

--- HSRG/components/imusensor/imu_sensor.cpp
+++ HSRG/components/imusensor/imu_sensor.cpp
@@ -43,8 +43,41 @@ extern "C" esp_err_t imu_init() {
     s_mpu.setSampleRate(100); // 새로운 클럭 주기 생성
     s_mpu.setAccelFullScale(mpud::ACCEL_FS_4G); // 가속도 범위 설정
     s_mpu.setGyroFullScale(mpud::GYRO_FS_500DPS); // 자이로 범위 생성
-    s_mpu.setDigitalLowPassFilter(mpud::DLPF_42HZ); // 노이즈 제거 및 값 안정화
+    s_mpu.setDigitalLowPassFilter(mpud::DLPF_42HZ); // 노이즈 제거 및 값 안정화mpud::raw_axes_t accelR
 
     ESP_LOGI(TAG, "Succeed to init mpu");
+    return ESP_OK;
+}
+
+extern "C" esp_err_t imu_get(float* ax, float* ay, float* az, float* gx, float*gy, float* gz) {
+    mpud::raw_axes_t accelRaw;
+    mpud::raw_axes_t gyroRaw;
+
+    esp_err_t res;
+
+    res = s_mpu.acceleration(&accelRaw);
+    if (res != ESP_OK) {
+        ESP_LOGE(TAG, "failed to get accelRaw");
+        return res;
+    }
+
+    res = s_mpu.rotation(&gyroRaw);
+    if (res != ESP_OK) {
+        ESP_LOGE(TAG, "failed to get gyroRaw");
+        return res;
+    }
+    ESP_LOGI(TAG, "Succeed to get accelRaw and gyroRaw");
+
+    auto accelG = mpud::accelGravity(accelRaw, mpud::ACCEL_FS_4G);
+    auto gyroDPS = mpud::gyroDegPerSec(gyroRaw, mpud::GYRO_FS_500DPS);
+
+    *ax = accelG.x;
+    *ay = accelG.y;
+    *az = accelG.z;
+
+    *gx = gyroDPS.x;
+    *gy = gyroDPS.y;
+    *gz = gyroDPS.z;
+
     return ESP_OK;
 }

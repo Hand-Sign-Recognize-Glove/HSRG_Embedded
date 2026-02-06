a0a570cc3d1874763266b340963f54daa7e69383
ilfpns <yeomsh09@gmail.com>
2026-02-06 19:15:11 +0900

    add Doxygen comments

--- HSRG/components/calculate/cal.h
+++ HSRG/components/calculate/cal.h
@@ -9,6 +9,26 @@
 #include "freertos/queue.h"
 #include "main_core.h"
 
+/**
+ * @brief 글자 계산 로직
+ * @param void
+ * @retval[in] None
+ */
 void cal_func(void);
+
+/**
+ * @brief 자료형을 float로 변환
+ * @param 
+ *      - int value : now flexsensor[i] 
+ *      - int min_val : fingers[i] min value
+ *      - int max_val : fingers[i] max value
+ * @retval float value
+ */
 float convert_to_float(int value, int min_val, int max_val);
+
+/**
+ * @brief 계산 메인 태스크
+ * @param void* pvParameters
+ * @retval[in] None
+ */
 void main_cal_task(void* pvParameters);
